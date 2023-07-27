bd30476df92947ed1fea9fe7ed7465af788986b1
mcgmatheus <matheus.mcg@hotmail.com.br>
2023-07-27 20:50:55 -0300

    Adiciona exercicios

--- Semana 8/Exercicios Praticos/Instagram clone/scripts/cadastro.js
+++ Semana 8/Exercicios Praticos/Instagram clone/scripts/cadastro.js
@@ -1,44 +1,84 @@
-import { regexEmail, regexTelefone } from '../utils/regexs.js'
+import { regexEmail, regexTelefone } from "../utils/regexs.js";
 
-document.getElementById('form-cadastro').addEventListener('submit', fazerCadastro)
+const campoNomeCompleto = document.getElementById("campo-nome-completo");
+const campoSenha = document.getElementById("campo-senha");
+const campoTelefone = document.getElementById("campo-telefone");
+const campoEmail = document.getElementById("campo-email");
+
+document
+  .getElementById("form-cadastro")
+  .addEventListener("submit", fazerCadastro);
 
 function fazerCadastro(event) {
+  event.preventDefault();
 
-    event.preventDefault()
+  const nome = campoNomeCompleto.value;
+  const senha = campoSenha.value;
+  const telefone = campoTelefone.value;
+  const email = campoEmail.value;
 
-    const nome = document.getElementById('campo-nome-completo').value
-    const senha = document.getElementById('campo-senha').value
-    const telefone = document.getElementById('campo-telefone').value
-    const email = document.getElementById('campo-email').value
+  campoNomeCompleto.classList.remove("input-error");
+  document.getElementById("error-nome").hidden = true;
 
-    document.getElementById('campo-nome-completo').classList.remove("input-error")
-    document.getElementById('error-nome').hidden = true
+  campoSenha.classList.remove("input-error");
+  document.getElementById("error-senha").hidden = true;
 
-    document.getElementById('campo-senha').classList.remove("input-error")
-    document.getElementById('error-senha').hidden = true
+  campoTelefone.classList.remove("input-error");
+  document.getElementById("error-telefone").hidden = true;
 
-    document.getElementById('campo-telefone').classList.remove("input-error")
-    document.getElementById('error-telefone').hidden = true
+  campoEmail.classList.remove("input-error");
+  document.getElementById("error-email").hidden = true;
 
-    document.getElementById('campo-email').classList.remove("input-error")
-    document.getElementById('error-email').hidden = true
+  if (nome === "") {
+    campoNomeCompleto.classList.add("input-error");
+    campoNomeCompleto.focus();
+    document.getElementById("error-nome").hidden = false;
+    document.getElementById("error-nome").innerText = "O nome é obrigatório";
+  } else if (senha.length < 9) {
+    campoSenha.classList.add("input-error");
+    campoSenha.hidden = false;
+    document.getElementById("error-senha").innerText =
+      "A senha deve no minimo 8 caracteres";
+  } else if (regexTelefone.test(telefone) === false) {
+    campoTelefone.classList.add("input-error");
+    document.getElementById("error-telefone").hidden = false;
+    document.getElementById("error-telefone").innerText =
+      "Digite um telefone válido";
+  } else if (regexEmail.test(email) === false) {
+    campoEmail.classList.add("input-error");
+    document.getElementById("error-email").hidden = false;
+    document.getElementById("error-email").innerText = "Digite um email válido";
+  } else {
+    //cadastrar dados que foram digitados
+    /*
+    GET - lista varias informações
+    POST- cadastra um novo recurso
+    DELETE - deletar um recurso
+    PUT - Atualiza todas as informações
+    PATCH - Atualiza uma informação
+    */
+    document.getElementById("login-button").style.opacity = 0.5;
+    document.getElementById("login-button").innerText = "Cadastrando....";
 
-    if (nome === "") {
-        document.getElementById('campo-nome-completo').classList.add('input-error')
-        document.getElementById('campo-nome-completo').focus()
-        document.getElementById('error-nome').hidden = false
-        document.getElementById('error-nome').innerText = "O nome é obrigatório"
-    } else if (senha.length < 9) {
-        document.getElementById('campo-senha').classList.add('input-error')
-        document.getElementById('error-senha').hidden = false
-        document.getElementById('error-senha').innerText = "A senha deve no minimo 8 caracteres"
-    } else if (regexTelefone.test(telefone) === false) {
-        document.getElementById('campo-telefone').classList.add('input-error')
-        document.getElementById('error-telefone').hidden = false
-        document.getElementById('error-telefone').innerText = "Digite um telefone válido"
-    } else if (regexEmail.test(email) === false) {
-        document.getElementById('campo-email').classList.add('input-error')
-        document.getElementById('error-email').hidden = false
-        document.getElementById('error-email').innerText = "Digite um email válido"
-    }
+    fetch("http://localhost:3333/usuarios", {
+      method: "POST",
+      body: JSON.stringify({
+        nome: nome,
+        senha: senha,
+        telefone: telefone,
+        email: email,
+      }),
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+    })
+      .then(() => {
+        alert("Cadastrado com sucesso");
+        window.location.href = "./index.html";
+      })
+      .catch(() => {
+        alert("Houve um erro ao cadastrar, tente novamente");
+      });
+  }
 }
